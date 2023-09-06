a4028a7ef999a3e80211cd6d341f3248c2160d97
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-09-07 02:32:43 +0300

    Text input field added.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export default function Home() {
+
+    const [text, setText] = useState('');
+
     return(
-        <div>
-            Home Page
+        <div className="flex flex-col justify-center items-center">
+            <textarea 
+                className="w-1/2 h-full bg-black opacity-50 mt-2 pl-1 hover:border-blue-400 rounded-lg resize-none text-white focus:border-blue-500 focus:shadow-blue-500 focus:shadow-md" 
+                value={text} cols={100} rows={Math.min(11, Math.max(4, Math.ceil(text.length / 92)))} maxLength={5000} onChange={(e) => setText(e.target.value)} 
+            />
         </div>
     )
 }
